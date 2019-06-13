13f679bc9aa132067d749969bea387223dd67301
xiaoyi <xiaoyi1221520@163.com>
2019-06-13 19:14:17 +0800

    modify

--- 2th.cpp
+++ 2th.cpp
@@ -13,6 +13,12 @@ int main(int argc, char** argv){
     
 	cout<<*it<<endl;
 //	(*it)++;    string类型是无法执行++运算符的，int型的可以
+//	*it.empty();//error  解引用运算符低于 .运算符
+
+	it->empty();
+	//++*it;//error   ++运算符高于解运算符
+
+	it++->empty();//++运算符高于->
     return 0;
 }
 
